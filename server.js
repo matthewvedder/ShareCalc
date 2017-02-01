1f38a30fe00b038fbe5ffdb636c7666caaef73bb
veddster <matthewvedder@gmail.com>
2017-02-01 18:53:00 -0500

    add and configure socektio

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 var path = require('path');
 var express = require('express');
 var app = express();
+var http = require('http').Server(app)
 var PORT = process.env.PORT || 8080
+var io = require('socket.io')(http)
+
+
+io.on('connection', function (socket) {
+  console.log('connection!')
+  socket.on('calculation', function (calculation) {
+    io.emit('calculation', calculation)
+  })
+})
 
 // using webpack-dev-server and middleware in development environment
 if(process.env.NODE_ENV !== 'production') {
@@ -10,7 +20,7 @@ if(process.env.NODE_ENV !== 'production') {
   var webpack = require('webpack');
   var config = require('./webpack.config');
   var compiler = webpack(config);
-  
+
   app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
   app.use(webpackHotMiddleware(compiler));
 }
@@ -21,7 +31,7 @@ app.get('/', function(request, response) {
   response.sendFile(__dirname + '/dist/index.html')
 });
 
-app.listen(PORT, function(error) {
+http.listen(PORT, function(error) {
   if (error) {
     console.error(error);
   } else {
--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,37 @@ import '../assets/stylesheets/base.scss';
 import React from 'react';
 import update from 'react-addons-update';
 import math from 'mathjs';
+import io from 'socket.io-client'
+const socket = io(process.env.PORT)
 
 
 const App = React.createClass({
   getInitialState: function() {
     return ({
-      operations: []
+      operations: [],
+      calculations: []
     });
   },
+  componentDidMount() {
+    socket.on('calculation', (calculation) => {
+      this.setState({ calculations: [...this.state.calculations, calculation] })
+      console.log(this.state.calculations)
+    })
+  },
+  mapList(array) {
+    return (
+      array.map((message, i) => {
+        return <p key={i}>{message}</p>
+      })
+    )
+  },
   calculateOperations: function() {
-    let result = this.state.operations.join('');
-    if (result) {
-      result = String(math.eval(result));
+    const operation = this.state.operations.join('');
+
+    if (operation) {
+      const result = String(math.eval(operation));
       this.setState({ operations : [result] });
+      socket.emit('calculation', `${operation} = ${result}`)
     }
   },
   handleClick: function(e) {
@@ -34,32 +52,35 @@ const App = React.createClass({
   },
   render: function() {
     return (
-      <div className="App">
-        <Display data={this.state.operations} />
-        <Buttons>
-          <Button onClick={this.handleClick} label="C" value="clear" />
-          <Button onClick={this.handleClick} label="7" value="7" />
-          <Button onClick={this.handleClick} label="4" value="4" />
-          <Button onClick={this.handleClick} label="1" value="1" />
-          <Button onClick={this.handleClick} label="0" value="0" />
+      <div>
+        <div className="App">
+          <Display data={this.state.operations} />
+          <Buttons>
+            <Button onClick={this.handleClick} label="C" value="clear" />
+            <Button onClick={this.handleClick} label="7" value="7" />
+            <Button onClick={this.handleClick} label="4" value="4" />
+            <Button onClick={this.handleClick} label="1" value="1" />
+            <Button onClick={this.handleClick} label="0" value="0" />
 
-          <Button onClick={this.handleClick} label="/" value="/" />
-          <Button onClick={this.handleClick} label="8" value="8" />
-          <Button onClick={this.handleClick} label="5" value="5" />
-          <Button onClick={this.handleClick} label="2" value="2" />
-          <Button onClick={this.handleClick} label="." value="." />
+            <Button onClick={this.handleClick} label="/" value="/" />
+            <Button onClick={this.handleClick} label="8" value="8" />
+            <Button onClick={this.handleClick} label="5" value="5" />
+            <Button onClick={this.handleClick} label="2" value="2" />
+            <Button onClick={this.handleClick} label="." value="." />
 
-          <Button onClick={this.handleClick} label="x" value="*" />
-          <Button onClick={this.handleClick} label="9" value="9" />
-          <Button onClick={this.handleClick} label="6" value="6" />
-          <Button onClick={this.handleClick} label="3" value="3" />
-          <Button label="" value="null" />
+            <Button onClick={this.handleClick} label="x" value="*" />
+            <Button onClick={this.handleClick} label="9" value="9" />
+            <Button onClick={this.handleClick} label="6" value="6" />
+            <Button onClick={this.handleClick} label="3" value="3" />
+            <Button label="" value="null" />
 
-          <Button onClick={this.handleClick} label="-" value="-" />
-          <Button onClick={this.handleClick} label="+" size="2" value="+" />
-          <Button onClick={this.handleClick} label="=" size="2" value="equal" />
-        </Buttons>
-      </div>
+            <Button onClick={this.handleClick} label="-" value="-" />
+            <Button onClick={this.handleClick} label="+" size="2" value="+" />
+            <Button onClick={this.handleClick} label="=" size="2" value="equal" />
+          </Buttons>
+        </div>
+      <Calculations mapList={this.mapList} calculations={this.state.calculations} />
+    </div>
     );
   }
 });
@@ -81,12 +102,19 @@ function Buttons (props) {
   )
 }
 
-function Button (props
-  ) {
+function Button (props) {
   return (
     <div onClick={props.onClick} className="Button" data-size={props.size} data-value={props.value}>
       {props.label}
     </div>
+  )
+}
+
+function Calculations ({ mapList, calculations }) {
+  return (
+    <ul>
+      { mapList(calculations) }
+    </ul>
   )
 }
 
